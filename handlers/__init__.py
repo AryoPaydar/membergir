7ee29f8e0acad3eca7d1f31c9121f839a09bfc28
AryoPaydar <aryo.english1996@gmail.com>
2026-09-19 00:10:02 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,6 @@
 from . import (
     user, admin, ads, transfer, referral, gift, shop,
     panel, orders_history, top, admin_shop, admin_texts, history,
+    admin_coins, admin_user_info, admin_complete, admin_channels,
+    admin_cancel, admin_transfer, admin_referral, admin_panels, admin_orders,
 )
