6bf94313341d63e1d93eba46d53f4030d5374a72
AryoPaydar <aryo.english1996@gmail.com>
2026-09-24 13:32:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from handlers import (
     panel, orders_history, top, admin_shop, admin_texts, history,
     admin_coins, admin_user_info, admin_complete, admin_channels,
     admin_cancel, admin_transfer, admin_referral, admin_panels, admin_orders,
+    admin_ads_channels,
 )
 from utils.keyboards import main_menu
 
